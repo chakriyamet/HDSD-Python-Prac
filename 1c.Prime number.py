0cc4f63a9eb657e5cf97204a9770aa9921681090
chakriya_met <metchakriya@gmail.com>
2022-07-11 16:55:09 +0700

    done

---
+++ 1c.Prime number.py
@@ -0,0 +1,17 @@
+# 1c.Prime number between 2 numbers 
+firstnum = 1
+secondnum = 100
+amount = 0
+prime = True
+
+for betnum in range (firstnum, secondnum+1):
+    if betnum > 1:
+        for primenum in range(2, betnum):
+            if betnum % primenum ==0:
+                prime = False
+                break
+        if(prime):
+            print(betnum)
+            ammount = amount + 1
+            
+        prime = True
