2aa5c1ae3df3690e1a1cec17ad56ecb3334682b1
chakriya_met <metchakriya@gmail.com>
2022-07-13 17:55:29 +0700

    done

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,12 +1,13 @@
 initial_list = [12,12,34,5,1,2,3,0,8]
 new_list =[]
 while initial_list:
-    mininum = initial_list[0]
+    minimum = initial_list[0]
     for i in initial_list:
-        if mininum > i:
-            mininum = 1
-    #new_list.insert(a, mininum)
-    new_list.append(mininum)
-    #initial_list.remove(mininum)
+        if minimum > i:
+            minimum = 1
+    #new_list.insert(a, minimum)
+    new_list.append(minimum)
+    initial_list.remove(minimum)
+    #initial_list.remove(minimum)
 print(new_list)
     
---
+++ array.py
@@ -0,0 +1,9 @@
+Number = 10
+
+mylist = [1,2,3,4,5,6,7,8,9,0]
+for i in mylist:
+    #print (i)
+    if i == Number:
+       print(" Found it")
+            
+        
