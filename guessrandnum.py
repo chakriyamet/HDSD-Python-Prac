9b212ad062008ea709a0a5a6ad70e8fb38b6409f
chakriya_met <metchakriya@gmail.com>
2022-08-05 15:01:41 +0700

    first version

---
+++ guessrandnum.py
@@ -0,0 +1,9 @@
+#guess the right number low/win
+import random as rand
+compnumber = rand.randint(1, 100)
+usernumber = int(input("Choose a number between 1 and 100. You'll get 5 guesses or you lose! "))
+
+if compnumber == usernumber:
+    print("You win!")
+else:
+    print("You lose!")
